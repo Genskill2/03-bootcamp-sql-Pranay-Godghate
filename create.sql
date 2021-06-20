4c5aa1278737c96884dbe1b5275b79875fcf31e3
Pranay godghate <pranay_b190560me@nitc.ac.in>
2021-06-20 13:55:33 +0530

    Creating table

--- create.sql
+++ create.sql
@@ -1,7 +1,23 @@
-PRAGMA foreign_keys;
-PRAGMA foreign_keys=ON;
-CREATE TABLE publisher(id INTEGER PRIMARY KEY,name TEXT NOT NULL,country TEXT NOT NULL);
-CREATE TABLE books(id INTEGER PRIMARY KEY,title TEXT NOT NULL,publisher INTEGER NOT NULL,FOREIGN KEY(publisher) REFERENCES publisher(id));
-CREATE TABLE subjects(id INTEGER PRIMARY KEY,name TEXT NOT NULL);
-CREATE TABLE books_subjects(book INTEGER NOT NULL,FOREIGN KEY(book) REFERENCES books(id),subject INTEGER NOT NULL,FOREIGN KEY(subject) REFERENCES subjects(id));
+PRAGMA foreign_keys = ON;
 
+
+CREATE TABLE publisher (
+      id integer primary key,
+      name text,
+      country text);
+
+CREATE TABLE books(
+      id integer primary key,
+      title text,
+      publisher integer references publisher(id)
+      );
+
+CREATE TABLE subjects(
+      id integer primary key,
+      name text);
+
+CREATE TABLE books_subjects(
+      book INTEGER references books(id),
+      subject INTEGER references subjects(id)
+      );
+      
